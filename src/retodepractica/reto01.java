da0dc53a3155c0827ef1613199fabea5b7b37788
andrea1615 <andreagracia1615@gmail.com>
2024-08-09 18:16:29 -0600

    fizz buzz

---
+++ src/retodepractica/reto01.java
@@ -0,0 +1,33 @@
+package retodepractica;
+
+public class reto01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 1;
+        while (x <= 100) {
+            if (x % 3 == 0 && x % 5 == 0) {
+                System.out.println("fizzbuzz");
+            } else if (x % 3 == 0) {
+                System.out.println("fizz");
+            } else if (x % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(x);
+            }
+            x++;
+            
+        }
+        
+	}
+	
+}
+            
+		
+		
+				
+				
+					
+        
+
+	
